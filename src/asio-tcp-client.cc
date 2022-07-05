6de12726c95622b91f5183cde68a9b1f00b4adef
Akshat Dubey <akshat@transcelestial.com>
2022-07-05 19:18:43 +0800

    feat: add asio async echo server

---
+++ src/asio-tcp-client.cc
@@ -0,0 +1,50 @@
+//
+// blocking_tcp_echo_client.cpp
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+// Copyright (c) 2003-2022 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include "asio.hpp"
+
+using asio::ip::tcp;
+
+enum { max_length = 1024 };
+
+int main(int argc, char* argv[]) {
+	try {
+		if (argc != 3) {
+			std::cerr << "Usage: blocking_tcp_echo_client <host> <port>\n";
+			return 1;
+		}
+
+		asio::io_context io_context;
+
+		tcp::socket   sock(io_context);
+		tcp::resolver resolver(io_context);
+		asio::connect(sock, resolver.resolve(argv[1], argv[2]));
+
+		std::cout << "Enter message: ";
+		char request[max_length];
+		std::cin.getline(request, max_length);
+		size_t request_length = std::strlen(request);
+		asio::write(sock, asio::buffer(request, request_length));
+
+		char   reply[max_length];
+		size_t reply_length = asio::read(sock, asio::buffer(reply, request_length));
+		std::cout << "Reply is: ";
+		std::cout.write(reply, reply_length);
+		std::cout << "\n";
+		std::cin.get();
+	} catch (std::exception& e) {
+		std::cerr << "Exception: " << e.what() << "\n";
+	}
+
+	return 0;
+}
