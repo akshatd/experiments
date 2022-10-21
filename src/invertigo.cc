95cde768905516a72bd5d450b28d3364315687f6
Akshat Dubey <akshat@transcelestial.com>
2022-10-21 17:59:05 +0800

    fix: fix some warnings in invertigo

--- src/invertigo.cc
+++ src/invertigo.cc
@@ -9,15 +9,15 @@
 #define N_COLS 3
 
 const char* input[N_ROWS][N_COLS] = {
-	{"abcd", "T", "Celestial"},
-	{"alewOwela", "Me", "needABeerNow"},
-	{"Anna", "AbsolutelY", "doOrDont"},
-	{"this ", "is", "fun?"},
+	{		 "abcd",          "T",    "Celestial"},
+	{"alewOwela",         "Me", "needABeerNow"},
+	{		 "Anna", "AbsolutelY",     "doOrDont"},
+	{		"this ",         "is",         "fun?"},
 };
 
-void invert_word(const char* input, char* output, size_t len);
-void print_const(const char* input[N_ROWS][N_COLS], size_t rows, size_t cols);
-void print_nonconst(char* input[N_ROWS][N_COLS], size_t rows, size_t cols);
+void invert_word(const char* in, char* out, size_t len);
+void print_const(const char* in[N_ROWS][N_COLS], size_t rows, size_t cols);
+void print_nonconst(char* in[N_ROWS][N_COLS], size_t rows, size_t cols);
 
 int main(void) {
 	printf("Input:\n");
@@ -40,31 +40,31 @@ int main(void) {
 	return 0;
 }
 
-void print_const(const char* input[][3], size_t rows, size_t cols) {
+void print_const(const char* in[][3], size_t rows, size_t cols) {
 	for (size_t row = 0; row < rows; ++row) {
 		for (size_t col = 0; col < cols; col++) {
-			printf("%s ", input[row][col]);
+			printf("%s ", in[row][col]);
 		}
 		printf("\n");
 	}
 }
 
-void print_nonconst(char* input[][3], size_t rows, size_t cols) {
+void print_nonconst(char* in[][3], size_t rows, size_t cols) {
 	for (size_t row = 0; row < rows; ++row) {
 		for (size_t col = 0; col < cols; col++) {
-			printf("%s ", input[row][col]);
+			printf("%s ", in[row][col]);
 		}
 		printf("\n");
 	}
 }
 
-void invert_word(const char* input, char* output, size_t len) {
+void invert_word(const char* in, char* out, size_t len) {
 	for (size_t i = 0; i < len; ++i) {
-		output[i] = input[len - i - 1];
+		out[i] = in[len - i - 1];
 	}
-	if (strcmp(input, output) == 0) {
+	if (strcmp(in, out) == 0) {
 		for (size_t i = 0; i < len; ++i) {
-			output[i] = toupper(output[i]);
+			out[i] = toupper(out[i]);
 		}
 	}
 }
